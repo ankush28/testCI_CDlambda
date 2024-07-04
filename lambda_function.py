15fb7842b75071b43b382ab62dce7f2d036b2008
Ankush choudhary <ankushchoudhary@jklu.edu.in>
2024-07-04 08:38:27 +0530

    yrsdy

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,8 @@
-# import requests
-
-# def lambda_handler(event, context):
-#     # TODO implement
-#     return {
-#         'statusCode': 200,
-#         'body': json.dumps('Hello from Lambda!')
-#     }
+import pandas as pd
 
 
-import requests
-
-def handler(event, context):
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-    res = {
-        "event": event,
-        "output": response.json(),
-        "context": context
-    }
-    print(res)
-
-    return None
+def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1')
