fbb859f72fe1e3cc6cc4da86f0d002cd51a75f7c
Ankush choudhary <ankushchoudhary@jklu.edu.in>
2024-07-05 09:56:06 +0530

    time change

--- lambda_function.py
+++ lambda_function.py
@@ -4,13 +4,12 @@ from openpyxl.styles import Font
 from datetime import datetime
 import os
 
-# Initialize boto3 clients
+
 dynamodb_client = boto3.client('dynamodb')
 cloudwatch_client = boto3.client('cloudwatch')
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    # Fetch DynamoDB tables
     tables = dynamodb_client.list_tables()['TableNames']
     
     # Create a workbook and a worksheet
@@ -41,11 +40,9 @@ def lambda_handler(event, context):
         # Append data to the worksheet
         ws.append([table_name, read_capacity, write_capacity, table_size])
     
-    # Save the workbook to a file
     file_name = f"/tmp/dynamodb_metrics_{datetime.now().strftime('%Y-%m-%d')}.xlsx"
     wb.save(file_name)
     
-    # Upload the file to S3
     s3_bucket = 'testwss3333'
     s3_key = f"dynamodb_reports/{os.path.basename(file_name)}"
     s3_client.upload_file(file_name, s3_bucket, s3_key)
@@ -56,7 +53,6 @@ def lambda_handler(event, context):
     }
 
 def get_consumed_capacity(table_name, metric_name):
-    # Fetch CloudWatch metrics for the specified table and metric
     response = cloudwatch_client.get_metric_statistics(
         Namespace='AWS/DynamoDB',
         MetricName=metric_name,
@@ -65,7 +61,7 @@ def get_consumed_capacity(table_name, metric_name):
         ],
         StartTime=datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0),
         EndTime=datetime.utcnow(),
-        Period=86400,
+        Period=60,
         Statistics=['Sum']
     )
     return response['Datapoints'][0]['Sum'] if response['Datapoints'] else 0
